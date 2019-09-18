761b7be8c052067831b9578c49496d37df66bd16
Avto Jaja <52544731+avtojaja@users.noreply.github.com>
2019-09-18 12:54:25 +0400

    Remove embedded col div and add padding.

--- src/registration.php
+++ src/registration.php
@@ -14,46 +14,44 @@
 <body class="registration">
     <div class="container">
         <div class="row justify-content-md-center my-5">
-            <div class="col-sm-6 border border-primary shadow rounded py-2">
-                <div class="col-sm-12">
-                    <h3 class="py-3">KEDEL-ზე რეგისტრაცია</h3>
-                    <form method="post" autocomplete="off">
+            <div class="col-sm-6 border border-primary shadow rounded p-4">
+                <h3 class="pb-3">KEDEL-ზე რეგისტრაცია</h3>
+                <form method="post" autocomplete="off">
+                    <div class="form-group">
+                        <label for="email" class="font-weight-bold">ელ. ფოსტა</label>
+                        <div class="input-group">
+                            <input type="email" name="email" id="email" class="form-control" placeholder="მიუთითეთ მოქმედი ელ. ფოსტა">
+                            <div class="input-group-append"><button type="button" class="btn btn-primary emailCheck"><i class="fa fa-envelope"></i></button></div>
+                        </div>
+                    </div>
+                    <div id="next-form" class="collapse">
                         <div class="form-group">
-                            <label for="email" class="font-weight-bold">ელ. ფოსტა</label>
-                            <div class="input-group">
-                                <input type="email" name="email" id="email" class="form-control" placeholder="მიუთითეთ მოქმედი ელ. ფოსტა">
-                                <div class="input-group-append"><button type="button" class="btn btn-primary emailCheck"><i class="fa fa-envelope"></i></button></div>
-                            </div>
+                            <label for="username" class="font-weight-bold">სახელი <small class="text-dark"><em>ეს იქნება შენი მომხმარებლის სახელი</em></small></label>
+                            <input type="text" name="username" id="username" class="form-control" placeholder="აირჩიეთ მომხმარებლის სახელი">
                         </div>
-                        <div id="next-form" class="collapse">
-                            <div class="form-group">
-                                <label for="username" class="font-weight-bold">სახელი <small class="text-dark"><em>ეს იქნება შენი მომხმარებლის სახელი</em></small></label>
-                                <input type="text" name="username" id="username" class="form-control" placeholder="აირჩიეთ მომხმარებლის სახელი">
-                            </div>
-                            <div class="form-group">
-                                <label for="phone" class="font-weight-bold">მობილური #</label>
-                                <input type="text" name="phone" id="phone" class="form-control" placeholder="(500)-(0000000)">
-                            </div>
-                            <div class="form-group">
-                                <label for="password" class="font-weight-bold">პაროლი</label>
-                                <input type="password" name="password" id="password" class="form-control" placeholder="***********">
-                            </div>
-                            <div class="form-group">
-                                <label for="cpassword" class="font-weight-bold">გაიმეორეთ პაროლი</label>
-                                <input type="password" name="cpassword" id="cpassword" class="form-control" placeholder="***********">
-                                <em id="cp"><span class="text-danger">პაროლები არ ემთხვევა!</span></em>
-                            </div>
-                            <div class="form-group">
-                                <input type="checkbox" name="condition" id="condition">
-                                <label for="condition">ვეთანხმები</label>
-                                <a href="#" target="_blank">რეგისტრაციის წესებს</a>
-                            </div>
-                            <div class="form-group">
-                                <button class="btn btn-success" type="submit">რეგისტრაცია</button>
-                            </div>
+                        <div class="form-group">
+                            <label for="phone" class="font-weight-bold">მობილური #</label>
+                            <input type="text" name="phone" id="phone" class="form-control" placeholder="(500)-(0000000)">
                         </div>
-                    </form>
-                </div>
+                        <div class="form-group">
+                            <label for="password" class="font-weight-bold">პაროლი</label>
+                            <input type="password" name="password" id="password" class="form-control" placeholder="***********">
+                        </div>
+                        <div class="form-group">
+                            <label for="cpassword" class="font-weight-bold">გაიმეორეთ პაროლი</label>
+                            <input type="password" name="cpassword" id="cpassword" class="form-control" placeholder="***********">
+                            <em id="cp"><span class="text-danger">პაროლები არ ემთხვევა!</span></em>
+                        </div>
+                        <div class="form-group">
+                            <input type="checkbox" name="condition" id="condition">
+                            <label for="condition">ვეთანხმები</label>
+                            <a href="#" target="_blank">რეგისტრაციის წესებს</a>
+                        </div>
+                        <div class="form-group">
+                            <button class="btn btn-success" type="submit">რეგისტრაცია</button>
+                        </div>
+                    </div>
+                </form>
             </div>
         </div>
     </div>
