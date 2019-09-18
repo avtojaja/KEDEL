803688114b60408ca5e0c3c02cf5300fd5f8d8ec
Avto Jaja <52544731+avtojaja@users.noreply.github.com>
2019-09-18 12:34:02 +0400

    Encapsulate jQuery.

--- src/static/js/main.js
+++ src/static/js/main.js
@@ -1,68 +1,70 @@
-$(function (e) {
-    $( '#username' ).on( 'keyup', function () {
-        if ( ! $( '#username' ).val().length ) {
-            $( '#username' ).addClass( 'is-invalid' );
-            return false;
-        } else {
-            $( '#username' ).removeClass( 'is-invalid' );
-        }
-    });
+(function ( $ ) {
+    $(function (e) {
+        $( '#username' ).on( 'keyup', function () {
+            if ( ! $( '#username' ).val().length ) {
+                $( '#username' ).addClass( 'is-invalid' );
+                return false;
+            } else {
+                $( '#username' ).removeClass( 'is-invalid' );
+            }
+        });
 
-    $( '#phone' ).on( 'keyup', function () {
-        if ( ! $( '#phone' ).val().length ) {
-            $( '#phone' ).addClass( 'is-invalid' );
-            return false;
-        } else {
-            $( '#phone' ).removeClass( 'is-invalid' );
-        }
-    });
+        $( '#phone' ).on( 'keyup', function () {
+            if ( ! $( '#phone' ).val().length ) {
+                $( '#phone' ).addClass( 'is-invalid' );
+                return false;
+            } else {
+                $( '#phone' ).removeClass( 'is-invalid' );
+            }
+        });
 
-    $( '#password' ).on( 'keyup', function () {
-        if ( ! $( '#password' ).val().length ) {
-            $( '#password' ).addClass( 'is-invalid' );
-            return false;
-        } else {
-            $( '#password' ).removeClass( 'is-invalid' );
-        }
-    });
+        $( '#password' ).on( 'keyup', function () {
+            if ( ! $( '#password' ).val().length ) {
+                $( '#password' ).addClass( 'is-invalid' );
+                return false;
+            } else {
+                $( '#password' ).removeClass( 'is-invalid' );
+            }
+        });
 
-    $( '#cpassword' ).on( 'keyup', function () {
-        if ( ! $( '#cpassword' ).val().length ) {
-            $( '#cpassword' ).addClass( 'is-invalid' );
-            return false;
-        } else {
-            $( '#cpassword' ).removeClass( 'is-invalid' );
-        }
-    });
+        $( '#cpassword' ).on( 'keyup', function () {
+            if ( ! $( '#cpassword' ).val().length ) {
+                $( '#cpassword' ).addClass( 'is-invalid' );
+                return false;
+            } else {
+                $( '#cpassword' ).removeClass( 'is-invalid' );
+            }
+        });
 
-    $( '.emailCheck' ).click(function () {
-        if ( ! $( '#email' ).val().length ) {
-            $( '#email' ).addClass( 'is-invalid' );
-            return false;
-        } else {
-            var regMail = /^([_a-zA-Z0-9-]+)(\.[_a-zA-Z0-9-]+)*@([a-zA-Z0-9-]+\.)+([a-zA-Z]{2,})$/;
-            if ( ! regMail.test( $( '#email' ).val() ) ) {
+        $( '.emailCheck' ).click(function () {
+            if ( ! $( '#email' ).val().length ) {
                 $( '#email' ).addClass( 'is-invalid' );
                 return false;
             } else {
-                $( '#email' ).removeClass( 'is-invalid' );
-                $( '#next-form' ).collapse( 'show' );
+                var regMail = /^([_a-zA-Z0-9-]+)(\.[_a-zA-Z0-9-]+)*@([a-zA-Z0-9-]+\.)+([a-zA-Z]{2,})$/;
+                if ( ! regMail.test( $( '#email' ).val() ) ) {
+                    $( '#email' ).addClass( 'is-invalid' );
+                    return false;
+                } else {
+                    $( '#email' ).removeClass( 'is-invalid' );
+                    $( '#next-form' ).collapse( 'show' );
+                }
             }
-        }
-    });
+        });
 
-    $( '.registration form' ).submit(function () {
-        if ( ! $( '#username, #phone, #password, #cpassword' ).val().length ) {
-            $( '#username, #phone, #password, #cpassword' ).addClass( 'is-invalid' );
-            return false;
-        } else {
-            $( '#username, #phone, #password, #cpassword' ).removeClass( 'is-invalid' );
-        }
+        $( '.registration form' ).submit(function () {
+            if ( ! $( '#username, #phone, #password, #cpassword' ).val().length ) {
+                $( '#username, #phone, #password, #cpassword' ).addClass( 'is-invalid' );
+                return false;
+            } else {
+                $( '#username, #phone, #password, #cpassword' ).removeClass( 'is-invalid' );
+            }
 
-        if ( $( '#password' ).val() !== $( '#cpassword' ).val() ) {
-            $( '#cpassword' ).addClass( 'is-invalid' );
-            $( '#cp' ).show();
-            return false;
-        }
+            if ( $( '#password' ).val() !== $( '#cpassword' ).val() ) {
+                $( '#cpassword' ).addClass( 'is-invalid' );
+                $( '#cp' ).show();
+                return false;
+            }
+        });
     });
-});
+})( jQuery );
